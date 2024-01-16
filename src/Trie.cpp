54d1d3bc19e2d7d597d2f9372aeb6406ae17612c
Kobe Li <likobe23@gmail.com>
2024-01-16 13:10:14 -0800

    Implement various functions

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -1,22 +1,47 @@
-#include "Trie.h"
+#include <vector>
 #include <unordered_map>
+
+#include "Trie.h"
+
 
 using namespace autosearch;
 
 char TRIE_NULL_CHAR = '\0';
 
 TrieNode::TrieNode() {
-    value = TRIE_NULL_CHAR;
-    is_word = false;
-    children = std::unordered_map<char, TrieNode>();
+    this->value = TRIE_NULL_CHAR;
+    this->is_word = false;
+    this->children = std::unordered_map<char, TrieNode>();
 }
 
 TrieNode::TrieNode(char value) {
     this->value = value;
-    is_word = false;
-    children = std::unordered_map<char, TrieNode>();
+    this->is_word = false;
+    this->children = std::unordered_map<char, TrieNode>();
 }
 
 Trie::Trie() {
-    head = TrieNode();
+    this->head = TrieNode();
+    this->words = std::vector<std::string>();
+}
+
+Trie::Trie(std::vector<std::string> words) {
+    this->head = TrieNode();
+    this->words = words;
+
+    for (int i = 0; i < words.size(); i++) {
+        this->insert(words[i]);
+    }
+}
+
+void Trie::insert(std::string word) {
+    TrieNode cursor = this->head;
+    for (int i = 0; i < word.length(); i++) {
+        if (!cursor.children.contains(word[i])) {
+            cursor.children.insert({word[i], TrieNode(word[i])});
+        }
+        cursor = cursor.children.at(word[i]);
+    }
+    // cursor now points at the node representing the last character in word
+    cursor.is_word = true;
 }
