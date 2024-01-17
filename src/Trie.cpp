bf3190ed6a9f89149d2fb321de70c60315043507
Kobe Li <likobe23@gmail.com>
2024-01-16 22:11:43 -0800

    Implement lookup function

--- src/Trie.cpp
+++ src/Trie.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 #include <unordered_map>
 
@@ -19,6 +20,18 @@ TrieNode::TrieNode(char value) {
     this->is_word = false;
     this->children = std::unordered_map<char, TrieNode>();
 }
+
+void TrieNode::collect_words(std::vector<std::string> &words) {
+    if (this->is_word) {
+        words.push_back(this->word);
+    }
+
+    // move onto child nodes
+    for (auto entry = this->children.begin(); entry != this->children.end(); entry++) {
+        entry->second.collect_words(words);
+    }
+}
+
 
 Trie::Trie() {
     this->head = TrieNode();
@@ -44,4 +57,21 @@ void Trie::insert(std::string word) {
     }
     // cursor now points at the node representing the last character in word
     cursor.is_word = true;
+    cursor.word = word;
+}
+
+std::vector<std::string> Trie::lookup(std::string prefix) {
+    std::vector<std::string> words = std::vector<std::string>();
+
+    TrieNode cursor = this->head;
+    for (int i = 0; i < prefix.length(); i++) {
+        if (!cursor.children.contains(prefix[i])) {
+            return words;
+        }
+        cursor = cursor.children.at(prefix[i]);
+    }
+    // cursor now points at the nod representing the last character in prefix
+    cursor.collect_words(words);
+
+    return words;
 }
--- src/Trie.h
+++ src/Trie.h
@@ -14,6 +14,13 @@ namespace autosearch {
         std::unordered_map<char, TrieNode> children;
         bool is_word;
         char value;
+        std::string word;
+
+        /*
+        If the TrieNode holds a word, adds it to the words collection
+        before moving onto the child nodes.
+        */
+        void collect_words(std::vector<std::string> &words);
     };
 
     class Trie {
@@ -21,8 +28,15 @@ namespace autosearch {
         Trie();
         Trie(std::vector<std::string> words);
 
+        /*
+        Insert the given word into the Trie.
+        */
         void insert(std::string word);
-        std::vector<std::string> lookup(std::string str);
+
+        /*
+        Returns a collection of words with the given prefix.
+        */
+        std::vector<std::string> lookup(std::string prefix);
 
     private:
         TrieNode head;
